dac2bc26ef8a2d20f70d95a73c04add78ee013fc
Ton <phantom.ness@windowslive.com>
2015-02-23 14:07:53 +0700

    init CollectionView

--- Podfile
+++
@@ -1,6 +0,0 @@
-# Uncomment this line to define a global platform for your project
-platform :ios, '7.0'
-
-target 'StampStampStamp' do
-
-end
---
+++ StampStampStamp/CollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  CollectionViewController.h
+//  StampStampStamp
+//
+//  Created by Tawatchai Sunarat on 2/23/15.
+//  Copyright (c) 2015 pddk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewController : UICollectionViewController
+
+@end
